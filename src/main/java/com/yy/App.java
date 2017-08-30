1d4284c9c2956506ccf7b55511f6a87138696b43
yy <2572836523@qq.com>
2017-08-30 16:10:28 +0800

    优化框架

--- src/main/java/com/yy/App.java
+++ src/main/java/com/yy/App.java
@@ -3,23 +3,33 @@ package com.yy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/web")
 public class App {
 
-	@GetMapping("/get")
+	@GetMapping("/name")
+	@ResponseBody
 	public String getName(){
 		return "yy";
 	}
 	
-	@GetMapping("/index")
+	@GetMapping("/html")
 	public String getHtml(){
 		return "index";
 	}
 	
-	@GetMapping("/page")
+	@GetMapping("/jsp")
 	public String get(){
 		return "page";
+	}
+	
+	@GetMapping("/mv")
+	public ModelAndView getPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("users/info");
+        return mv;
 	}
 }
